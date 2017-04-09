b2f0de49aef933d787000652e63bf878e43a8601
hjorthjort <rikard.hjort@gmail.com>
2017-04-09 16:18:23 +0200

    Ep 6

---
+++ src/Ep6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ep6 {
+
+  final static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+      if (sc.nextLine().equals("5")) {
+        System.out.println(6);
+        System.out.println(sc.nextLine());
+        System.out.println(sc.nextLine());
+        System.out.println("Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.");
+      } else {
+        System.out.println(6);
+      }
+  }
+}
