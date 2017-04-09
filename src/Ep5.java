9de1d8ae450467acaeb799b840c3e0abc6b18e45
hjorthjort <rikard.hjort@gmail.com>
2017-04-09 16:05:50 +0200

    Refactor 4 and 5

--- src/Ep5.java
+++ src/Ep5.java
@@ -1,18 +1,18 @@
+import java.util.Scanner;
+
 public class Ep5 {
 
   final static String TRUE_STRING = "A long time ago, in a galaxy far, far away ...";
+  final static Scanner sc = new Scanner(System.in);
 
   public static void main(String[] args) {
-
-    try {
-      if (args[1].equals(TRUE_STRING)) {
-        System.out.println("\"" + TRUE_STRING + "\"");
-        System.out.println("\n\"It is a period of civil war\"");
+      sc.nextLine();
+      String l0 = sc.nextLine();
+      if (l0.equals(TRUE_STRING)) {
+        System.out.println(TRUE_STRING);
+        System.out.println("\nIt is a period of civil war");
       } else {
         System.out.println(5);
       }
-    } catch (Exception e) {
-      System.out.println("kkrrzzrrzrz. [Exit]");
-    }
   }
 }
