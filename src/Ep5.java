59ab7715f9e4559aca52be824d3372ab841edfb9
hjorthjort <rikard.hjort@gmail.com>
2017-04-09 16:15:39 +0200

    use identifying numbers

--- src/Ep5.java
+++ src/Ep5.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 
 public class Ep5 {
 
-  final static String TRUE_STRING = "A long time ago, in a galaxy far, far away ...";
   final static Scanner sc = new Scanner(System.in);
 
   public static void main(String[] args) {
-      sc.nextLine();
-      String l0 = sc.nextLine();
-      if (l0.equals(TRUE_STRING)) {
-        System.out.println(TRUE_STRING);
-        System.out.println("\nIt is a period of civil war");
+      if (sc.nextLine().equals("4")) {
+        System.out.println("5");
+        System.out.println(sc.nextLine());
+        System.out.println("It is a period of civil war.");
       } else {
         System.out.println(5);
       }
